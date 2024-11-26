2f6e5438f06b4b9063c13393d3e020bc195bfb14
Jonathan Gardner <jonathangardnermd@outlook.com>
2024-11-26 10:53:49 -0600

    cleaned up comments

--- test.py
+++ test.py
@@ -7,30 +7,6 @@ from pydantic import BaseModel
 ERROR_EVENT = "error"
 
 
-# def handleError(func):
-#     def wrapper(*args, **kwargs):
-#         try:
-#             return func(*args, **kwargs)
-#         except Exception as e:
-#             error_message = exceptionToStr(e)
-#             emitter = getEmitter()
-#             emitter.emit(ERROR_EVENT, error_message)
-
-#     return wrapper
-
-
-# def handleErrorAsync(func):
-#     async def wrapper(*args, **kwargs):
-#         try:
-#             return await func(*args, **kwargs)
-#         except Exception as e:
-#             error_message = exceptionToStr(e)
-#             emitter = getEmitter()
-#             emitter.emit(ERROR_EVENT, error_message)
-
-#     return wrapper
-
-
 async def run():
 
     # use the logging module
